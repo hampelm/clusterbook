23b898a91043f26be45076d30b909c7cf2514a94
Matt Hampel <matth@Matt-Hampel.local>
2010-04-19 12:10:54 -0400

    Updated links to media

--- clusterbook/configs/common/settings.py
+++ clusterbook/configs/common/settings.py
@@ -46,7 +46,7 @@ MEDIA_ROOT = os.path.join(SITE_ROOT, 'assets')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://d3-test-1.s3.amazonaws.com/clusterbook/'
+MEDIA_URL = 'http://clusterbook.s3.amazonaws.com/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
